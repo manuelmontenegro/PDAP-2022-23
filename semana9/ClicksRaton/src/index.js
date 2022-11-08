bbf710705b63cbceb8b2622fdb654790cd84d1a7
Manuel Montenegro <mmontene@ucm.es>
2022-11-08 21:12:56 +0100

    Añadido zip

--- semana9/ClicksRaton/src/index.js
+++ semana9/ClicksRaton/src/index.js
@@ -1,12 +1,19 @@
 
-import { fromEvent, map, filter, take } from 'rxjs';
+import { fromEvent, map, filter, take, scan, zip } from 'rxjs';
 
 const area = document.getElementById('areaClicks');
 
-const click$ = fromEvent(area, 'click').pipe(
+
+const coord$ = fromEvent(area, 'click').pipe(
     map(evt => ({x: evt.offsetX, y: evt.offsetY})),
-    filter(({x, y}) => x < area.clientWidth / 2),
-    take(10)
+    filter(({x, y}) => x < area.clientWidth / 2)
 );
 
-click$.subscribe(({x, y}) => console.log(`Click en (${x},${y})`));
+const cont$ = coord$.pipe(scan((ac, _) => ac + 1, 0));
+
+zip(coord$, cont$).pipe(
+        take(10)
+    ).subscribe(([{x, y}, cont]) => {
+        console.log(`Pulsación en (${x}, ${y}) / contador = ${cont}`);
+    });
+
