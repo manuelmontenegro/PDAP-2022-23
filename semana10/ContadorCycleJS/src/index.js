ac0e5e1923f26a31e77ac5817ed55ad7d1ad3e36
Manuel Montenegro <montenegro@fdi.ucm.es>
2022-11-23 12:51:19 +0100

    Refactorización contador con Cycle.js

--- semana10/ContadorCycleJS/src/index.js
+++ semana10/ContadorCycleJS/src/index.js
@@ -1,14 +1,19 @@
 import './rxjs-adapter.js'
 import { run } from '@cycle/run'
-import { button, div, makeDOMDriver } from '@cycle/dom'
+import { button, div, makeDOMDriver, h } from '@cycle/dom'
 import { startWith, map, scan, merge } from 'rxjs'
 
 
 function main(sources) {
-    const incrementar$ = sources.DOM.select('.incrementar').events('click').pipe(map(evt => +1));
-    const decrementar$ = sources.DOM.select('.decrementar').events('click').pipe(map(evt => -1));
+    const evtIncrementar$ = sources.DOM.select('button.incrementar').events('click');
+    const evtDecrementar$ = sources.DOM.select('button.decrementar').events('click');
 
-    const contador$ = merge(incrementar$, decrementar$).pipe(
+    const incrementar$ = evtIncrementar$.pipe(map(evt => +1));
+    const decrementar$ = evtDecrementar$.pipe(map(evt => -1));
+
+    const accion$ = merge(incrementar$, decrementar$);
+
+    const contador$ = accion$.pipe(
         startWith(0),
         scan((ac, x) => ac + x)
     );
@@ -16,9 +21,9 @@ function main(sources) {
     const dom$ = contador$.pipe(
         map(contador =>
             div([
-                button('.incrementar', '+'),
+                button('.incrementar','+'),
                 div('.contador', contador.toString()),
-                button('.decrementar', { attrs: {disabled: contador === 0 } }, '-')
+                button('.decrementar', { props: { disabled: contador === 0 } }, '-')
             ])
         )
     );
